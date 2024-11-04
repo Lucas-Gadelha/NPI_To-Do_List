85d61cc89d751c09f25452e008b28a90399c4deb
Núcleo de Práticas em Informática <lgfonseca@alu.ufc.br>
2024-11-04 12:36:50 -0300

    atualização 04.11

--- ToDoList/src/components/createTodo.vue
+++ ToDoList/src/components/createTodo.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+
+import { TodoStatus, type Todo } from "@/types"
+import { reactive, ref } from 'vue';
+
 const displayForm = ref(false);
+
+const newTodo = reactive<Omit<Todo, "id">>({
+    title: "",
+    description: "",
+    status: TodoStatus.Pending,
+});
+
+const resetForm () => {
+    displayForm.value = false;
+    newTodo.title = "";
+    newTodo.description = "";
+}
 </script>
 
 <template>
@@ -8,8 +24,15 @@ const displayForm = ref(false);
         <template v-else>
             <form>
                 <div>
-                    <input type="text" placeholder="Título">
+                    <input type="text" placeholder="Título" v-model="newTodo.title" />
                 </div>
+                <div>
+                    <textarea type="text" placeholder="Título" v-model="newTodo.title" />
+                </div>
+
+                <button type="submit">Cadastrar</button>
+                <button type="button" @click="resetForm">Cancelar</button>
+
                 <div>
                     <input type="text" placeholder="Descrição">
                 </div>
